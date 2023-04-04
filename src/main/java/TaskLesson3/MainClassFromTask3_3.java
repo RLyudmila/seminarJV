bc9e9a5c6426f26655bbace49235336da660e4c9
RLyudmila <116638126+RLyudmila@users.noreply.github.com>
2023-04-04 13:44:01 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/TaskLesson3/MainClassFromTask3_3.java
+++ src/main/java/TaskLesson3/MainClassFromTask3_3.java
@@ -1,5 +1,22 @@
 package TaskLesson3;
 
+import java.util.*;
+
 public class MainClassFromTask3_3 {
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 10, 1, 2, 2, 3, 3, 10, 3, 4, 5, 5));
+        System.out.println("Массив с дубликатами " + list);
+        ArrayList<Integer> newList = removeDuplicates(list);
+        System.out.println("Массив с удаленными дубликатами " + newList);
+    }
+    public static <T> ArrayList<T> removeDuplicates(ArrayList<T> list) {
+        ArrayList<T> newList = new ArrayList<T>();
+        for (T element : list) {
+            if (!newList.contains(element)) {
+                newList.add(element);
+            }
+        }
+        return newList;
+    }
 
 }
