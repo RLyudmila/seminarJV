e6b541fda5085bf212db2ab1cfeec6ae0fc744fa
RLyudmila <116638126+RLyudmila@users.noreply.github.com>
2023-04-12 15:46:58 +0300

    task-6

---
+++ src/main/java/Lesson6/MainClassFromLesson6_4.java
@@ -0,0 +1,12 @@
+package Lesson6;
+import java.util.*;
+public class MainClassFromLesson6_4 {
+    // TreeSet
+    public static void main(String[] args) {
+        var a = new TreeSet<>(Arrays.asList(1,7,2,3,6,4,5));
+        var b = new TreeSet<>(Arrays.asList(13,3,17,7,2,11,5));
+        System.out.println(a); // [1, 2, 3, 4, 5, 6, 7]
+        System.out.println(b); // [2, 3, 5, 7, 11, 13, 17]
+        System.out.println(a.contains(1)); // true
+    }
+}
